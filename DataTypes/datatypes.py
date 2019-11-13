bb7ccd1aa5ad4d5c30a884cfc8d4dade6cd5cd30
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-11-14 08:38:58 +0900

    numbers

--- DataTypes/datatypes.py
+++ DataTypes/datatypes.py
@@ -23,3 +23,23 @@ j = True
 print(type(j))
 k = b"Hello"
 print(type(k))
+l = bytearray(5)
+print(type(l))
+m = memoryview(bytes(5))
+print(type(m))
+
+# Setting a specific data type
+x = str("Hello World")
+x = int(20)
+x = float(20.5)
+x = complex(1j)	
+x = list(("apple", "banana", "cherry"))
+x = tuple(("apple", "banana", "cherry"))	
+x = range(6)
+x = dict(name="John", age=36)
+x = set(("apple", "banana", "cherry"))	
+x = frozenset(("apple", "banana", "cherry"))	
+x = bool(5)
+x = bytes(5)	
+x = bytearray(5)	
+x = memoryview(bytes(5))
---
+++ Numbers/numbers.py
@@ -0,0 +1,10 @@
+#There are three numeric types in Python:
+#int
+#float
+#complex
+x = 1 # int
+y = 3.14 # float
+z = 1j # complex
+print(type(x))
+print(type(y))
+print(type(z))

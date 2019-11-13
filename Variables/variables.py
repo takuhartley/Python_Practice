d15fb3d0b1500f63bb0518c4e878d0abdb5a823f
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-11-14 08:23:35 +0900

    Global key

--- Variables/variables.py
+++ Variables/variables.py
@@ -69,3 +69,19 @@ def RapperSounds():
   print(x)
 RapperSounds()
 print(x)
+
+# The global keyword
+# Normally, 
+# when you create a variable inside a function, 
+# that variable is local,
+# and can only be used inside that function.
+# To create a global variable inside a function,
+# you can use the global keyword.
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)

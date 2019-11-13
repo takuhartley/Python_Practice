98d142c960db901e6c45723efa12041eeaeb0000
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-11-14 08:12:45 +0900

    Variables

---
+++ Variables/variables.py
@@ -0,0 +1,28 @@
+# Can be a number
+a = 1
+print(a)
+b = 2
+print(b)
+
+# Can be a string
+c = "Hello World!"
+print(c)
+
+# Can reassign variables
+c = "Robert"
+print(c)
+
+"""
+Rules for Python Variables
+A variable must start with a letter or underscore character
+Variable name cannot stop with a number
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+Variable names are case-sensitive (money, Money and MONEY are three different variables)
+"""
+
+# Assign value to multiple variables
+a, b, c, d = "Red", "Blue", "Green", "Yellow"
+print(a)
+print(b)
+print(c)
+print(d)

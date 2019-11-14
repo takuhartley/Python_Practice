eff9f65eaeaf028e30d62ccab1716e2634c64f17
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-11-15 08:56:58 +0900

    string methods expandtabs

--- Strings/stringmethods.py
+++ Strings/stringmethods.py
@@ -13,3 +13,23 @@ print(x)
 txt = "I love apples, apple are my favorite fruit"
 x = txt.count("apple")
 print(x)
+
+txt = "My name is Ståle"
+print(txt.encode)
+print(txt.encode(encoding="ascii",errors="backslashreplace"))
+print(txt.encode(encoding="ascii",errors="ignore"))
+print(txt.encode(encoding="ascii",errors="namereplace"))
+print(txt.encode(encoding="ascii",errors="replace"))
+print(txt.encode(encoding="ascii",errors="xmlcharrefreplace"))
+
+txt = "Hello, welcome to my world."
+x = txt.endswith(".")
+print(x)
+x = txt.endswith("my world.")
+print(x)
+x = txt.endswith("my world.", 5, 11)
+print(x)
+
+txt = "H\te\tl\tl\to"
+x =  txt.expandtabs(2)
+print(x)

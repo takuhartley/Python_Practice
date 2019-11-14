6dd34952ae1358eaea4ff410b5e18cbe0449934c
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-11-15 08:52:08 +0900

    strings

---
+++ Strings/stringmethods.py
@@ -0,0 +1,11 @@
+txt = "hello, and welcome to my world."
+x = txt.capitalize()
+print (x)
+
+txt = "Hello, And Welcome To My World!"
+x = txt.casefold()
+print(x)
+
+txt = "banana"
+x = txt.center(20)
+print(x)
--- Strings/strings.py
+++ Strings/strings.py
@@ -104,4 +104,4 @@ txt = "\110\145\154\154\157"
 print(txt) 
 
 txt = "\x48\x65\x6c\x6c\x6f"
-print(txt) 
+print(txt)

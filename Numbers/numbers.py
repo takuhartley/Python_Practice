42c3e1ea0d00bc0297390ccd577fef55dd8fef15
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-11-15 08:40:42 +0900

    numbers

--- Numbers/numbers.py
+++ Numbers/numbers.py
@@ -8,3 +8,30 @@ z = 1j # complex
 print(type(x))
 print(type(y))
 print(type(z))
+
+# Int, or integer, is a whole number, positive or negative, without decimals, of unlimited length.
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Float, or "floating point number" is a number, positive or negative, containing one or more decimals.
+x = 1.10
+y = 1.0
+z = -35.59
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Float can also be scientific numbers with an "e" to indicate the power of 10.
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))

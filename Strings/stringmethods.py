00710ce59e894008b162948c8b6cb412c48c0cdb
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-11-15 09:15:45 +0900

    string methods format

--- Strings/stringmethods.py
+++ Strings/stringmethods.py
@@ -36,3 +36,22 @@ print(x)
 print(txt.expandtabs(2))
 print(txt.expandtabs(4))
 print(txt.expandtabs(10))
+
+txt = "Hello, welcome to my world."
+x = txt.find("welcome")
+print(x)
+
+txt = "Hello, welcome to my world."
+x = txt.find("e")
+print(x)
+
+txt = "Hello, welcome to my world."
+x = txt.find("e", 5, 10)
+print(x)
+
+txt = "Hello, welcome to my world."
+print(txt.find("q"))
+print(txt.index("q"))
+
+txt = "For only {price:.2f} dollars!"
+print(txt.format(price = 49))

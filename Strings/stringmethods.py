ce54938c799e378fde91cbc93160f366a3459d29
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-11-16 14:15:00 +0900

    string methods rpartition

--- Strings/stringmethods.py
+++ Strings/stringmethods.py
@@ -194,3 +194,11 @@ print(x)
 txt = "banana"
 x = txt.rjust(20, "O")
 print(x)
+
+txt = "I could eat bananas all day, bananas are my favorite fruit"
+x = txt.rpartition("bananas")
+print(x)
+
+txt = "I could eat bananas all day, bananas are my favorite fruit"
+x = txt.rpartition("apples")
+print(x)

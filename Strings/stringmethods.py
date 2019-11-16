281ed31b72fa666d1cefb71169af4015ea4b19d2
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-11-16 14:11:44 +0900

    string methods rindex

--- Strings/stringmethods.py
+++ Strings/stringmethods.py
@@ -170,3 +170,24 @@ print(x)
 txt = "one one was a race horse, two two was one too."
 x = txt.replace("one", "three", 2)
 print(x)
+
+txt = "Mi casa, su casa."
+x = txt.rfind("casa")
+print(x)
+
+txt = "Hello, welcome to my world."
+x = txt.rfind("e")
+print(x)
+
+txt = "Mi casa, su casa."
+x = txt.rindex("casa")
+print(x)
+
+txt = "Hello, welcome to my world."
+x = txt.rindex("e")
+print(x)
+
+txt = "Hello, welcome to my world."
+x = txt.rindex("e", 5, 10)
+print(x)
+

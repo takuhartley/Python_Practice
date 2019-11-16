81c42909c848ff1083db59b57cdd4e8ee19bcd40
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-11-16 14:18:10 +0900

    string methods split

--- Strings/stringmethods.py
+++ Strings/stringmethods.py
@@ -218,3 +218,12 @@ print(x)
 txt = "hello, my name is Peter, I am 26 years old"
 x = txt.split(", ")
 print(x)
+
+txt = "apple#banana#cherry#orange"
+x = txt.split("#")
+print(x)
+
+txt = "apple#banana#cherry#orange"
+# setting the maxsplit parameter to 1, will return a list with 2 elements!
+x = txt.split("#", 1)
+print(x)

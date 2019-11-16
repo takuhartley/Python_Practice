002d6052e85099384483ce736de2669d2c483ff2
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-11-16 14:01:50 +0900

    string methods replace

--- Strings/stringmethods.py
+++ Strings/stringmethods.py
@@ -162,3 +162,11 @@ print(x)
 txt = "I like bananas"
 x = txt.replace("bananas", "apples")
 print(x)
+
+txt = "one one was a race horse, two two was one too."
+x = txt.replace("one", "three")
+print(x)
+
+txt = "one one was a race horse, two two was one too."
+x = txt.replace("one", "three", 2)
+print(x)

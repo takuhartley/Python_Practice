3b00c76109f81c5217dc349520144cb1a1964344
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-11-18 08:53:49 +0900

    question mark operators

--- Operators/assignmnet_operators.py
+++ Operators/assignmnet_operators.py
@@ -13,3 +13,39 @@ print(x)
 x *= 10
 print(x)
 
+# Division
+x /= 4
+print(x)
+
+# Modulus
+x %= 8
+print(x)
+
+# Exponentiation
+x **= 2
+print(x)
+
+# Floor
+x //= 3
+print(x)
+
+# I have no idea what these actually do
+x = 5
+x &= 3
+print(x)
+
+x = 5
+x |= 3
+print(x)
+
+x ^= 3
+print(x)
+
+x = 5
+x >>= 3
+print(x)
+
+x = 5
+x <<= 3
+print(x)
+

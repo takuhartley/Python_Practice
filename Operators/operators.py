ed2f05239bd4b8168bed07f0f66748f2c7cf045c
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-11-18 08:40:05 +0900

    Operators

--- Operators/operators.py
+++ Operators/operators.py
@@ -1,4 +1,36 @@
-x = 1
-y = 2
-z = x + y
-print(z)
+a = 1
+b = 2
+c = a + b
+print(c)
+
+a = 10
+b = 1
+c = a - b
+print(c)
+
+a = 5
+b = 10
+c = a * b
+print(c)
+
+a = 10
+b = 2
+c = a / b
+print(c)
+
+a = 19
+b = 2
+c = a % b
+print(c)
+
+a = 10
+b = 2
+c = a ** b
+print(c)
+
+a = 15
+b = 2
+c = a // b
+print(c)
+
+

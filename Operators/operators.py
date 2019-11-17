e600b3d198c9e42cce80c1babb4ae5f38a27c6cc
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-11-18 08:42:27 +0900

    Operator disc

--- Operators/operators.py
+++ Operators/operators.py
@@ -1,36 +1,43 @@
+# Python Operators
+
+# Addition
 a = 1
 b = 2
 c = a + b
 print(c)
 
+# Substraction
 a = 10
 b = 1
 c = a - b
 print(c)
 
+# Multiplication
 a = 5
 b = 10
 c = a * b
 print(c)
 
+# Division
 a = 10
 b = 2
 c = a / b
 print(c)
 
+# Modulus
 a = 19
 b = 2
 c = a % b
 print(c)
 
+# Exponentiation
 a = 10
 b = 2
 c = a ** b
 print(c)
 
+# Floor
 a = 15
 b = 2
 c = a // b
 print(c)
-
-

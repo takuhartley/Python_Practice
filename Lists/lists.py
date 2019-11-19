5641e17b95cb3737732dad42310d965480bed846
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-11-19 22:07:03 +0900

    range of list

--- Lists/lists.py
+++ Lists/lists.py
@@ -3,3 +3,9 @@ print(thislist)
 
 thislist = ["apple", "banana", "cherry"]
 print(thislist[1])
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])

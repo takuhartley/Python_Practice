fabb537e237d86b4acabdeb3bc3b45c60d7551cd
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-11-19 22:08:11 +0900

    negative index

--- Lists/lists.py
+++ Lists/lists.py
@@ -9,3 +9,6 @@ print(thislist[-1])
 
 thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
 print(thislist[2:5])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[-4:-1])

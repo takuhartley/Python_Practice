7cd9c71decc0e979dbd75546a58294a26f71321e
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-11-19 22:02:58 +0900

    membership operators

--- Operators/comparison_operators.py
+++ Operators/comparison_operators.py
@@ -34,3 +34,17 @@ z = x
 print(x is z)
 print(x is y)
 print(x == y)
+
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+
+print(x is not z)
+print(x is not y)
+print(x != y)
+
+x = ["apple", "banana"]
+print("banana" in x)
+
+x = ["apple", "banana"]
+print("pineapple" not in x)

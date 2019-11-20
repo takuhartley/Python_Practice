e7cedbe7a883dcb262acbef6fdd20c540c4978dd
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-11-20 21:40:12 +0900

    updating list

--- Lists/lists.py
+++ Lists/lists.py
@@ -12,3 +12,8 @@ print(thislist[2:5])
 
 thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
 print(thislist[-4:-1])
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+

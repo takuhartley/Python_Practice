3690464f6a68d7f767d61ecd5b9d873bf92f26e3
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-11-24 22:36:13 +0900

    changing tuple into list

--- Tuples/tuples.py
+++ Tuples/tuples.py
@@ -9,3 +9,13 @@ print(thistuple[-1])
 
 thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
 print(thistuple[2:5])
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-4:-1])
+
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+
+print(x)

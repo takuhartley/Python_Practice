79b1d5aaa39b16e9827709fa05745cf4417ffd6f
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-11-24 22:54:40 +0900

    check if item exists

--- Tuples/tuples.py
+++ Tuples/tuples.py
@@ -23,3 +23,7 @@ print(x)
 thistuple = ("apple", "banana", "cherry")
 for x in thistuple:
   print(x)
+
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple")

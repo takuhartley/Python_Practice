7711dbb312109e7cb16a852ae00127f3f7901304
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-11-24 22:26:27 +0900

    tuples

--- Tuples/tuples.py
+++ Tuples/tuples.py
@@ -1,2 +1,8 @@
 thistuple = ("apple", "banana", "cherry")
 print(thistuple)
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[-1])

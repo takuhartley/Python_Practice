28ce06a8cd46842500fb75c1ede1b1d844683efa
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-11-24 22:27:04 +0900

    tuple indexes

--- Tuples/tuples.py
+++ Tuples/tuples.py
@@ -6,3 +6,6 @@ print(thistuple[1])
 
 thistuple = ("apple", "banana", "cherry")
 print(thistuple[-1])
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])

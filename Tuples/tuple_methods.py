91ef91357d3b5108269d9fef06957e937e0bd256
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-11-25 21:52:35 +0900

    index()

--- Tuples/tuple_methods.py
+++ Tuples/tuple_methods.py
@@ -1,6 +1,8 @@
 # count()
-thistuple = (1, 3, 7, 8, 7, 5, 4, 6, 8, 5)
+a = (1, 3, 7, 8, 7, 5, 4, 6, 8, 5)
+b = a.count(5)
+print(b)
 
-x = thistuple.count(5)
-
-print(x)
+# index()
+c = a.index(8)
+print(c)

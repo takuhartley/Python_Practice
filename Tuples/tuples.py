49a2603860415af682a857a1c2c07d1dfa0afb92
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-11-25 21:46:26 +0900

    tuple length

--- Tuples/tuples.py
+++ Tuples/tuples.py
@@ -27,3 +27,6 @@ for x in thistuple:
 thistuple = ("apple", "banana", "cherry")
 if "apple" in thistuple:
   print("Yes, 'apple' is in the fruits tuple")
+
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))

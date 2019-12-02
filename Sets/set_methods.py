483429b0c68c8fa7eb9dc937a7410688b587ac0c
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-12-02 22:15:35 +0900

    intersection_update()

--- Sets/set_methods.py
+++ Sets/set_methods.py
@@ -33,3 +33,8 @@ x = {"apple", "banana", "cherry"}
 y = {"google", "microsoft", "apple"}
 z = x.intersection(y)
 print(z)
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+x.intersection_update(y)
+print(x)

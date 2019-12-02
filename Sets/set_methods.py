c015029440287d7104935f6ad701d95dfa659a33
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-12-02 22:16:04 +0900

    isdisjoint()

--- Sets/set_methods.py
+++ Sets/set_methods.py
@@ -38,3 +38,8 @@ x = {"apple", "banana", "cherry"}
 y = {"google", "microsoft", "apple"}
 x.intersection_update(y)
 print(x)
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "facebook"}
+z = x.isdisjoint(y)
+print(z)

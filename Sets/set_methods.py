65d9c79f918badf97c8fb77974979e7e791dbcf1
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-12-02 22:15:00 +0900

    intersection()

--- Sets/set_methods.py
+++ Sets/set_methods.py
@@ -12,27 +12,24 @@ print(a)
 
 x = {"apple", "banana", "cherry"}
 y = {"google", "microsoft", "apple"}
-
 z = x.difference(y)
-
 print(z)
 
 x = {"apple", "banana", "cherry"}
 y = {"google", "microsoft", "apple"}
-
 z = y.difference(x)
-
 print(z)
 
 x = {"apple", "banana", "cherry"}
 y = {"google", "microsoft", "apple"}
-
 x.difference_update(y)
-
 print(x)
 
 fruits = {"apple", "banana", "cherry"}
-
 fruits.discard("banana")
-
 print(fruits)
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+z = x.intersection(y)
+print(z)

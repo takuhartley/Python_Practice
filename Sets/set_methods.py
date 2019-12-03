ab3660ab7c0c0d89d586d6cf42492450f161d221
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-12-03 21:30:27 +0900

    union()

--- Sets/set_methods.py
+++ Sets/set_methods.py
@@ -71,3 +71,8 @@ x = {"apple", "banana", "cherry"}
 y = {"google", "microsoft", "apple"}
 x.symmetric_difference_update(y)
 print(x)
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+z = x.union(y)
+print(z)

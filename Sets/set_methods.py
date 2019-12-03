c519e385e46a44fe1c10ebdb8511a868d1804c16
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-12-03 21:30:01 +0900

    symmetric_difference_update()

--- Sets/set_methods.py
+++ Sets/set_methods.py
@@ -66,3 +66,8 @@ x = {"apple", "banana", "cherry"}
 y = {"google", "microsoft", "apple"}
 z = x.symmetric_difference(y)
 print(z)
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+x.symmetric_difference_update(y)
+print(x)

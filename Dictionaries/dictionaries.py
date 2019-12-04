07602894fa417917b8a5498b4df5de44798c4070
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-12-04 21:34:12 +0900

    check is key exists

--- Dictionaries/dictionaries.py
+++ Dictionaries/dictionaries.py
@@ -30,3 +30,11 @@ for x in thisdict.values():
 
 for x, y in thisdict.items():
   print(x, y)
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+if "model" in thisdict:
+  print("Yes, 'model' is one of the keys in the thisdict dictionary")

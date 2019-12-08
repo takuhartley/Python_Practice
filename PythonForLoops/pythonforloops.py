9031d3b7b7b7d69aef9eb2025d91cebbc8b4c216
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-12-09 08:23:13 +0900

    nested loops:

--- PythonForLoops/pythonforloops.py
+++ PythonForLoops/pythonforloops.py
@@ -36,3 +36,10 @@ for x in range(6):
   print(x)
 else:
   print("Finally finished!")
+
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)

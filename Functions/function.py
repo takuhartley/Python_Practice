cf97eb457858d97008652b86b5fdb9a57ed830b6
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-12-16 22:00:32 +0900

    default parameter values

--- Functions/function.py
+++ Functions/function.py
@@ -11,3 +11,11 @@ def function_two(fname):
 function_two("Cameron")
 function_two("Alex")
 function_two("Robert")
+
+# Default Parameter Values
+def function_three(country = "Japan"):
+  print("I am from " + country)
+
+function_three()
+
+

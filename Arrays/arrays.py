3942b23aa80c093af563cff265f46f7feb1e97d8
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-12-16 23:26:00 +0900

    Arrays

---
+++ Arrays/arrays.py
@@ -0,0 +1,19 @@
+cars = ["Honda", "Subaru", "Toyota", "Nissan"]
+
+a = cars[0]
+b = cars[1]
+c = cars[2]
+d = cars[3]
+e = len(cars)
+
+print(cars)
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+for x in cars:
+  print(x)
+
+cars.append("Tesla")
--- Lamdba/lamdba.py
+++ Lamdba/lamdba.py
@@ -1,52 +1,52 @@
 # Lamdba
 
 
-def x(a): return a + 10
+def function_one(a): return a + 10
 
 
-print(x(5))
+print(function_one(5))
 
 
-def x(a, b): return a * b
+def function_two(a, b): return a * b
 
 
-print(x(5, 6))
+print(function_two(5, 6))
 
 
-def x(a, b, c): return a + b + c
+def function_three(a, b, c): return a + b + c
 
 
-print(x(5, 6, 2))
-
-
-def function_one(n):
-    return lambda a: a * n
-
-
-def function_two(n):
-    return lambda a: a * n
-
-
-mydoubler = function_two(2)
-
-print(mydoubler(11))
-
-
-def function_three(n):
-    return lambda a: a * n
-
-
-mytripler = function_three(3)
-
-print(mytripler(11))
+print(function_three(5, 6, 2))
 
 
 def function_four(n):
     return lambda a: a * n
 
 
-mydoubler = function_four(2)
-mytripler = function_four(3)
+def function_five(n):
+    return lambda a: a * n
+
+
+mydoubler = function_five(2)
+
+print(mydoubler(11))
+
+
+def function_six(n):
+    return lambda a: a * n
+
+
+mytripler = function_six(3)
+
+print(mytripler(11))
+
+
+def function_seven(n):
+    return lambda a: a * n
+
+
+mydoubler = function_seven(2)
+mytripler = function_seven(3)
 
 print(mydoubler(11))
 print(mytripler(11))

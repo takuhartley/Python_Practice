a358501ccf06e7de4a1e3edd642471a9ee940128
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-12-17 22:27:16 +0900

    playing with classes

--- Classes/classes.py
+++ Classes/classes.py
@@ -1,19 +1,23 @@
 class Person:
-    def __init__(self, name, age):
-        self.name = name
+    def __init__(self, fname, lname, age):
+        self.first_name = fname
+        self.last_name = lname
         self.age = age
 
     def PersonIntroduction(self):
-        print("Hello my name is " + self.name)
+        print("Hello my name is " + self.first_name)
 
     def PersonAgeIntroduction(self):
         print("My age is " + str(self.age))
 
 
-p1 = Person("Robert", 22)
+p1 = Person("Robert", "Hartley", 22)
 p1.PersonIntroduction()
 p1.PersonAgeIntroduction()
 
 
 class Student(Person):
     pass
+
+x = Student("Cameron", "Hartley", 24)
+x.PersonIntroduction()

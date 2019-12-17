6f8c55d9ab44ea27f00b1167d0529204ad7e590b
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-12-17 22:20:23 +0900

    Playing with classes

--- Classes/classes.py
+++ Classes/classes.py
@@ -1,5 +1,14 @@
-class FirstClass:
-    x = 5
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-p1 = FirstClass()
-print(p1.x)
+    def PersonIntroduction(self):
+        print("Hello my name is " + self.name)
+
+    def PersonAgeIntroduction(self):
+        print("My age is " + str(self.age))
+
+p1 = Person("Robert", 22)
+p1.PersonIntroduction()
+p1.PersonAgeIntroduction()

e0e8d013316522877e07c08cafd092fd12dd1d2b
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-12-17 21:57:19 +0900

    array methods

--- Arrays/arrays.py
+++ Arrays/arrays.py
@@ -16,7 +16,35 @@ print(e)
 for x in cars:
   print(x)
 
-cars.append("Tesla")
-cars.pop(1)
-cars.remove("Honda")
+# Adds an element at the end of the list
+cars.append()
+
+# Removes all the elements from the list
 cars.clear()
+
+# Returns a copy of the list
+cars.copy()
+
+# Returns the number of elements with the specified value
+cars.count()
+
+# Add the elements of a list (or any iterable), to the end of the current list
+cars.extend()
+
+# Returns the index of the first element with the specified value
+cars.index()
+
+# Adds an element at the specified position
+cars.insert()
+
+# Removes the element at the specified position
+cars.pop()
+
+# Removes the first item with the specified value
+cars.remove()
+
+# Reverses the order of the list
+cars.reverse()
+
+# Sorts the list
+cars.sort()

553beecaf806a5fb9ac6f65696d3d3c072cbf4aa
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-12-17 22:03:48 +0900

    methods updated

--- Arrays/arrays.py
+++ Arrays/arrays.py
@@ -14,37 +14,47 @@ print(d)
 print(e)
 
 for x in cars:
-  print(x)
+    print(x)
 
 # Adds an element at the end of the list
-cars.append()
+cars.append("Ford")
+print(cars)
 
 # Removes all the elements from the list
 cars.clear()
 
 # Returns a copy of the list
-cars.copy()
+f = cars.copy()
+print(f)
 
 # Returns the number of elements with the specified value
-cars.count()
-
+g = cars.count("Ford")
+print(g)
 # Add the elements of a list (or any iterable), to the end of the current list
-cars.extend()
+people = ['Cameron', 'Robert', 'Alex', 'Clifford']
+h = people.extend(cars)
+print(h)
 
 # Returns the index of the first element with the specified value
-cars.index()
+i = cars.index("Honda")
+print(i)
 
 # Adds an element at the specified position
-cars.insert()
+j = people.insert(1, "Mark")
+print(j)
 
 # Removes the element at the specified position
 cars.pop()
 
 # Removes the first item with the specified value
 cars.remove()
+cars.remove("Honda")
+print(cars)
 
 # Reverses the order of the list
 cars.reverse()
+print(cars)
 
 # Sorts the list
 cars.sort()
+print(cars)

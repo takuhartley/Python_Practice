7d754d1aca5769f213710ea94776b8e4ecfb0e5e
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-12-18 09:10:19 +0900

    commented out json things

--- Json/json.py
+++ Json/json.py
@@ -3,10 +3,10 @@ import json as j
 
 # parse x:
 # y = json.loads(t.x)
-y = j.dumps(c.x)
-print(y)
-json.dumps(x, indent=4)
-json.dumps(x, indent=4, separators=(". ", " = "))
-json.dumps(x, indent=4, sort_keys=True)
+# y = j.dumps(c.x)
+# print(y)
+# json.dumps(x, indent=4)
+# json.dumps(x, indent=4, separators=(". ", " = "))
+# json.dumps(x, indent=4, sort_keys=True)
 # the result is a Python dictionary:
 # print(y["age"])

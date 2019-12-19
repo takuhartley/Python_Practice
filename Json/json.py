cbb48a30ff58b72c89b0c9ce0fede3ef7897751a
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-12-19 09:14:27 +0900

    Django

--- Json/json.py
+++ Json/json.py
@@ -4,9 +4,4 @@ import json as j
 # parse x:
 # y = json.loads(t.x)
 # y = j.dumps(c.x)
-# print(y)
-# json.dumps(x, indent=4)
-# json.dumps(x, indent=4, separators=(". ", " = "))
-# json.dumps(x, indent=4, sort_keys=True)
-# the result is a Python dictionary:
-# print(y["age"])
+# # print(y["age"])

848eb58441de21b66fe1119ae2e024bee9b763b1
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-12-22 00:00:16 +0900

    updated

--- MachineLearning/machinelearning_study.py
+++ MachineLearning/machinelearning_study.py
@@ -23,27 +23,18 @@
 # Mean Medium Mode
 # Mean
 import numpy
-
 speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
-
 x = numpy.mean(speed)
-
 print(x)
 
 # Medium
 import numpy
-
 speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
-
 x = numpy.median(speed)
-
 print(x)
 
 # Mode
 from scipy import stats
-
 speed = [99,86,87,88,111,86,103,87,94,78,77,85,86]
-
 x = stats.mode(speed)
-
 print(x)

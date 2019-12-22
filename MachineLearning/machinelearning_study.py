54d1ca2aeec2ca17fad7a4f4736200a32e10341f
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-12-23 08:56:15 +0900

    Standard Deviation Numpy

--- MachineLearning/machinelearning_study.py
+++ MachineLearning/machinelearning_study.py
@@ -43,3 +43,7 @@ print(x)
 # Number that describes how spread out the values are.
 # A low standard deviation means that most of the numbers are close to the mean (average) value.
 # A high standard deviation means that the values are spread out over a wider range.
+import numpy
+speed = [86,87,88,86,87,85,86]
+x = numpy.std(speed)
+print(x)

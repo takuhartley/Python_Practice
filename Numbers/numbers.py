39d340547507f1d4d206f85ed956d65554668de6
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2020-01-01 20:15:58 +0900

    Updating numbers

--- Numbers/numbers.py
+++ Numbers/numbers.py
@@ -1,37 +1,45 @@
-#There are three numeric types in Python:
-#int
-#float
-#complex
-x = 1 # int
-y = 3.14 # float
-z = 1j # complex
+# There are three numeric types in Python:
+# int
+# float
+# complex
+
+x = 1  # int
 print(type(x))
-print(type(y))
-print(type(z))
+
+x = 3.14  # float
+print(type(x))
+
+x = 1j  # complex
+print(type(x))
 
 # Int, or integer, is a whole number, positive or negative, without decimals, of unlimited length.
 x = 1
-y = 35656222554887711
-z = -3255522
-
 print(type(x))
-print(type(y))
-print(type(z))
+
+x = 35656222554887711
+print(type(x))
+
+x = -3255522
+print(type(x))
+
 
 # Float, or "floating point number" is a number, positive or negative, containing one or more decimals.
 x = 1.10
-y = 1.0
-z = -35.59
-
 print(type(x))
-print(type(y))
-print(type(z))
+
+x = 1.0
+print(type(x))
+
+x = -3.49
+print(type(x))
+
 
 # Float can also be scientific numbers with an "e" to indicate the power of 10.
 x = 35e3
-y = 12E4
-z = -87.7e100
-
 print(type(x))
-print(type(y))
-print(type(z))
+
+x = 12E4
+print(type(x))
+
+x = -87.7e100
+print(type(x))

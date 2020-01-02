73e85fdcb3af578b9867cebc291ac666934153db
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2020-01-02 22:34:26 +0900

    It's true

--- ifelses/ifelses.py
+++ ifelses/ifelses.py
@@ -1,7 +1,7 @@
 a = 33
 b = 200
 c = 300
-
+d = 0
 if b > a:
   print("b is greater than a")
 
@@ -9,3 +9,6 @@ if a + b > c:
   print("That shit dope")
 else:
   print("This shit tight")
+
+if d == 0:
+  print("IT'S TRUE")

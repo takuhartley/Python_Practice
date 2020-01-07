d256c7a2f8f2fa1814543f4a626ac7c89b9522d1
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2020-01-08 08:56:57 +0900

    Redoing question

--- Django/mysite/polls/views.py
+++ Django/mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from .models import Question
 from django.template import loader
@@ -10,10 +10,8 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):

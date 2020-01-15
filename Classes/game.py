58b9c652e39cedf923e5df3222fb705318bbda25
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2020-01-15 09:45:53 +0900

    Interesting classes

---
+++ Classes/game.py
@@ -0,0 +1,36 @@
+#class God:
+#     def __init__(self, n, s, a, m, h):
+#         self.name = n
+#         self.strength = s
+#         self.abilities = a
+#         self.mortal = m
+#         self.health = h
+
+#     def isMortal(self):
+#         self.mortal = False
+
+# class Demon:
+#     def __init__(self, n, s, a, m, h):
+#         self.name = n
+#         self.strength = s
+#         self.abilities = a
+#         self.mortal = m
+#         self.health = h
+
+#     def isMortal(self):
+#         self.mortal = False
+
+class Hero:
+    def __init__(self, n, s, a, h):
+        self.name = n
+        self.strength = s
+        self.abilities = a
+        self.mortal = True
+        self.health = h
+
+h1 = Hero("Elon", 75, "Super Strength", 100)
+print(h1.name)
+print(h1.strength)
+print(h1.abilities)
+print(h1.mortal)
+print(h1.health)
